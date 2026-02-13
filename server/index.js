70e279d4bf1c6cbd57fe54d76b593d000dc28b67
Saif Ullah <saifisaifbangash143@gmail.com>
2026-02-13 17:57:51 +0500

    Fix client routing with simple vercel.json rewrite

--- server/index.js
+++ server/index.js
@@ -22,7 +22,7 @@ app.use(
     origin: [
       process.env.FRONTEND_URL,
       "http://localhost:3000",
-      "http://localhost:5173"  // local Vite dev server
+      "http://localhost:5173", // local Vite dev server
     ].filter(Boolean),
   }),
 );
